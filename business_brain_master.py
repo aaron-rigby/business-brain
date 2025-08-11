1f7a96415a6ee0a346a3245556d1a852ac19ca52
aaron-rigby <riggaz15@gmail.com>
2025-08-11 08:55:56 +0700

    Update business_brain_master.py

--- business_brain_master.py
+++ business_brain_master.py
@@ -15,6 +15,37 @@ import plotly.graph_objects as go
 from typing import Dict, List, Optional
 import os
 
+# Password Protection
+def check_password():
+    """Returns `True` if the user had the correct password."""
+    
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets.get("APP_PASSWORD", "YourDefaultPassword"):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct
+        return True
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True
 # Page Configuration
 st.set_page_config(
     page_title="🧠 Business Brain Master",
